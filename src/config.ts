8acc051d8ab853e8c5e5f60da61cb49e636f3e25
nyungnim <nyu0kim@gmail.com>
2025-06-03 02:34:16 +0900

    add deploy url

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
     title: "NyungLog",  // 블로그 이름
     description: "IT Service Product Manager",  // 블로그 설명
     author: "녕님",  // 작성자 이름
-    url: "https://nana-blog.netlify.app",  // 실제 배포 URL (Netlify 이후 수정)
+    url: "https://nyung.netlify.app",  // 실제 배포 URL (Netlify 이후 수정)
     githubUrl: "https://github.com/nyungnim",  // 본인 GitHub
     twitter: "",  // SNS 계정 (없으면 빈 문자열로 놔둬도 됨)
   };
