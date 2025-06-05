d36a0ea65f35340b5e6c27664408d65afa564307
nyungnim <nyu0kim@gmail.com>
2025-06-06 01:14:46 +0900

    chore: update site metadata

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
     title: "NyungLog",  // 블로그 이름
-    description: "IT Service Product Manager",  // 블로그 설명
+    description: "개발하는 서비스 기획자의 성장기록",  // 블로그 설명
     author: "녕님",  // 작성자 이름
     url: "https://nyung.netlify.app",  // 실제 배포 URL (Netlify 이후 수정)
     githubUrl: "https://github.com/nyungnim",  // 본인 GitHub
